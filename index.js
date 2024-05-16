facd06445f5a7b74e95e3840113631126df96454
Tayler <taylerblevins@gmail.com>
2024-05-15 21:12:18 -0400

    done

--- index.js
+++ index.js
@@ -20,31 +20,31 @@
             },
             body : JSON.stringify(recipeCard)
         })
-        .then(response => {
+        /*.then(response => {
 
             if(!response.ok){
                 throw new Error("I can not submit the recipe, Sorry!")
             }
-        })
+        })*/
         .then (response => response.json())
-        .then(response => createCards(response))
-        .catch(console.error())
+        .then(data => createCards(data))
+        .catch(console.error)
     }
     // Get Data from json server 
     function RecipeBook(){
         fetch('http://localhost:3000/Recipes')
-        .then(response => {
+        /*.then(response => {
             if(!response.ok){
                 throw new Error("I can't find the Recipes, Sorry!")
             }
-        })
+        })*/
         .then(response => response.json())
         .then(data => {
-            const Recipes = data;
+            Recipes = data;
             console.log(Recipes);
-            Recipes.forEach(recipe => createCards(recipe))
+            Recipes.forEach(Recipe => createCards(Recipe))
         })
-        .catch(console.error())
+        .catch(console.error)
     }
 //Interaction with the DOM
     //Making recipe cards 
@@ -59,10 +59,11 @@
             <p>${Recipes.cooked}</p>
         </div>
         <div>
-        <button id="cooked"> I Made This </button>
-        <button id="delete"> Delete me </button>
+        <button class="cooked"> I Made This </button>
+        <button class="delete"> Delete me </button>
         </div>
         `
+        document.querySelector('#container').appendChild(card);S
         //document.getElementById('delete').addEventListener('click', console.log('delete'))
     }
 
